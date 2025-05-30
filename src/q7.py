fe5d5c838ba6b1e34b6f49b7289f84a364f342e9
hydrocort <fongkarhoe@gmail.com>
2025-05-31 00:03:01 +0800

    Add Car class with test cases

--- src/q7.py
+++ src/q7.py
@@ -5,11 +5,30 @@ class Car:
     - Initialize these attributes in the __init__ method
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
+    def __init__(self, make, model, year):
+        """
+        Constructor method that sets up the initial attributes of each car
+        """
+        self.make = make    
+        self.model = model  
+        self.year = year    
 
     def describe_car(self):
-        print("")
-
+        """
+        This method prints information about the car in the format "Year Make Model"
+        """
+        print(f"{self.year} {self.make} {self.model}")
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+
+print("=== Testing Car Class ===")
+print()
+
+# Create a new car object using our Car class blueprint
+my_car = Car("Toyota", "Corolla", 2020)
+
+# Call the describe_car method to display the car information
+print("Car description:")
+my_car.describe_car()
